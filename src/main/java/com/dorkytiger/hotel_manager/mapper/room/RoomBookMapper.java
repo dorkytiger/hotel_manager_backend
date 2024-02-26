c642f6277915797a027bd5305051dddc78120fc7
dorkytiger <604440452@qq.com>
2024-02-26 17:40:44 +0800

    init

--- src/main/java/com/dorkytiger/hotel_manager/mapper/room/RoomBookMapper.java
+++ src/main/java/com/dorkytiger/hotel_manager/mapper/room/RoomBookMapper.java
@@ -2,6 +2,8 @@ package com.dorkytiger.hotel_manager.mapper.room;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dorkytiger.hotel_manager.model.room.RoomBookEntity;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RoomBookMapper extends BaseMapper<RoomBookEntity> {
 }
--- src/main/java/com/dorkytiger/hotel_manager/mapper/user/UserInfoMapper.java
+++ src/main/java/com/dorkytiger/hotel_manager/mapper/user/UserInfoMapper.java
@@ -3,7 +3,8 @@ package com.dorkytiger.hotel_manager.mapper.user;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dorkytiger.hotel_manager.model.user.UserInfoEntity;
+import org.apache.ibatis.annotations.Mapper;
 
-
+@Mapper
 public interface UserInfoMapper extends BaseMapper<UserInfoEntity> {
 }
